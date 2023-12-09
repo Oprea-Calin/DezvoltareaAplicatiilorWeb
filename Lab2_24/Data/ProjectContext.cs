833b25b8d0f58d7efefe9fa216b3bd0660ac8ee0
OpreaCalin <opreaopreacalin@gmail.com>
2023-12-09 15:38:57 +0200

    Add project files.

---
+++ Lab2_24/Data/ProjectContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab2_24.Data
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext>options) : base(options)
+            {
+            }
+
+    }
+}
